f31b6e5a666142c306d176b725a85c453fdd9d21
guilhermeclamego <guilhermecesarl20@gmail.com>
2026-08-21 22:09:05 -0300

    Configure spring boot application

---
+++ src/main/java/io/github/guilhermeclamego/arqonsearch/playground/ArqonSearchPlaygroundApplication.java
@@ -0,0 +1,15 @@
+package io.github.guilhermeclamego.arqonsearch.playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArqonSearchPlaygroundApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(
+                ArqonSearchPlaygroundApplication.class,
+                args
+        );
+    }
+}
